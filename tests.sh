2f7e53f28416e51efde2a7b10294edf00b6dcdfe
Flo <cenestpasmonmail@hidden.truc>
2023-02-25 18:25:40 +0100

    aha

--- tests.sh
+++ tests.sh
@@ -1,12 +1,12 @@
 #!/bin/sh
 
 total=0
-success=0
+good=0
 for test in tests/testFile/*; do
     if [ -f "$test" ]; then
         ./bin/http_parse $test mot > /dev/null 2>&1
         if [ "$?" -eq 1 ]; then
-            success=$(($success+1))
+            good=$(($good+1))
         else
             ./bin/http_parse $test mot
             echo "./bin/http_parse $test mot"
@@ -16,4 +16,4 @@ for test in tests/testFile/*; do
     fi
 done
 
-echo "Succeded $success/$total tests."
+echo "Succeded $good/$total tests."
