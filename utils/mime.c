c5c70b0fd125bf4ff7cec47fe264af18052acd7d
flolep2607 <24566964+flolep2607@users.noreply.github.com>
2023-05-07 16:01:38 +0200

    up

--- utils/mime.c
+++ utils/mime.c
@@ -7,3 +7,15 @@ const char *get_file_extension(const char *filename) {
     }
     return dot + 1;
 }
+const char *get_file_type(const char *filename) {
+    const char *extension = get_file_extension(filename);
+    size_t num_mime_types = sizeof(MIME) / sizeof(MIME[0]);
+
+    for (size_t i = 0; i < num_mime_types; i++) {
+        if (strcasecmp(extension, MIME[i][0] + 1) == 0) {
+            return MIME[i][1];
+        }
+    }
+
+    return "unknown";
+}
