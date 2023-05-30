de8c3da3112f2a58c56fb1037fded55a81515797
flolep2607 <24566964+flolep2607@users.noreply.github.com>
2023-05-30 15:20:37 +0200

    o

--- utils/thirdpart.c
+++ utils/thirdpart.c
@@ -1,64 +1,71 @@
 #include "thirdpart.h"
 #include <stdio.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <unistd.h>
+#include <errno.h>
+#include <sys/types.h>
+#include <sys/socket.h>
+#include <netinet/in.h>
+#include <arpa/inet.h>
 
-int run_php(char *script_path, char **output) {
-    char *command = "php";
-    char *script_arg = script_path;
-    char *pipe_cmd = "/bin/bash -c 'popen(\"%s %s\",\"r\")'";
+#include "fastcgi.h"
 
-    FILE *pipe = popen(pipe_cmd, "r");
-    if (!pipe) {
-        perror("popen");
-        return 1;
-    }
+#define FCGI_SOCKET_PORT 9000
 
-    char *input = command;
-    while (*script_arg) {
-        if (*script_arg == '\"') {
-            script_arg++;
-            fputs(input, pipe);
-            input = script_arg;
-        } else {
-            fputc(*script_arg, pipe);
-            script_arg++;
+char* run_cgi(char* request) {
+    int fd = createSocket(FCGI_SOCKET_PORT);
+
+    // Send FastCGI request
+    sendGetValue(fd);
+    sendBeginRequest(fd, 10, FCGI_RESPONDER, FCGI_KEEP_CONN);
+    sendStdin(fd, 10, request, strlen(request));
+    sendData(fd, 10, request, strlen(request));
+
+    // Read FastCGI response
+    char* response = NULL;
+    size_t response_size = 0;
+    ssize_t read_size;
+
+    while ((read_size = read(fd, response + response_size, FCGI_HEADER_SIZE)) > 0) {
+        response_size += read_size;
+        response = realloc(response, response_size + FCGI_HEADER_SIZE);
+
+        FCGI_Header* header = (FCGI_Header*)(response + response_size - read_size);
+
+        if (header->type == FCGI_STDOUT || header->type == FCGI_STDERR) {
+            ssize_t content_length = ntohs(header->contentLength);
+            ssize_t padding_length = header->paddingLength;
+
+            // Read content data
+            while (content_length > 0) {
+                read_size = read(fd, response + response_size, content_length);
+                if (read_size <= 0)
+                    break;
+
+                response_size += read_size;
+                response = realloc(response, response_size + content_length);
+                content_length -= read_size;
+            }
+
+            // Read padding data
+            while (padding_length > 0) {
+                read_size = read(fd, response + response_size, padding_length);
+                if (read_size <= 0)
+                    break;
+
+                response_size += read_size;
+                response = realloc(response, response_size + padding_length);
+                padding_length -= read_size;
+            }
+        } else if (header->type == FCGI_END_REQUEST) {
+            break;
         }
     }
-    fputs(input, pipe);
 
-    if (pclose(pipe) == -1) {
-        perror("pclose");
-        return 1;
-    }
+    // Close the socket
+    close(fd);
 
-    *output = NULL;
-    if (output) {
-        while (fgets(*output, 4096, pipe) != NULL) {
-        }
-    }
-
-    return 0;
+    return response;
 }
-
-
-
-
-// #include <stdio.h>
-// #include <stdlib.h>
-// #include <fcgi_stdio.h>
-
-// int fastcgi_handler(struct FastCGI_Handler *handler) {
-//     char *query;
-//     int query_len;
-//     char *content;
-
-//     while (FCGI_Accept(handler) >= 0) {
-//         query = FCGI_GetParameter(handler, "QUERY_STRING", 0, &query_len);
-//         content = "Hello, World!";
-
-//         FCGI_SetHeader(handler, "Content-type", "text/html");
-//         FCGI_Output(handler, (char *)content, strlen(content));
-//         FCGI_SetHeader(handler, "Content-Length", query_len + strlen(content));
-//     }
-
-//     return 0;
-// }
